f050a57150e84807debd733b7e90477106be0585
sonia <smenon@wethod.com>
2023-04-20 23:29:00 +0200

    test

--- tina/config.ts
+++ tina/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tinacms";
 import { defaultFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 export default defineConfig({
   branch,
