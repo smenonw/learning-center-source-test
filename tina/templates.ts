dd273566ad53208231a8c802825d63f87d8d01a7
sonia <smenon@wethod.com>
2023-05-09 00:31:54 +0200

    test

--- tina/templates.ts
+++ tina/templates.ts
@@ -22,7 +22,7 @@ export function defaultFields() {
               type: 'string',
               required: true,
               ui: {
-                component: 'input',
+                component: 'text',
               },
             },
             {
